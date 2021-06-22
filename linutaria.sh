665fbf5e96c5cec6a7d72ce1af9f77533191262d
Eric Nunez <66031055+daiyamon@users.noreply.github.com>
2021-06-22 12:30:16 -0400

    Add files via upload

---
+++ linutaria.sh
@@ -0,0 +1,15 @@
+clear
+echo "Hello"
+read -p "Please enter your Hero Name: " name
+clear
+echo "Welcome $name"
+echo " Your Adventure as a Hero Begins Today"
+now=$(date)
+echo "$now"
+sleep 2
+echo " ▌│█║▌║▌║ Linutaria ║▌║▌║█│▌"
+echo "        【﻿ＳＴＡＲＴ】"
+echo " ▌│█║▌║▌║▌║▌║█│ ▌│█║▌║▌║█│▌"
+echo "Goodbye $name"
+sleep 2
+
